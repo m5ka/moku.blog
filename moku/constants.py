bfd37041d91d4849215dc21e0e92cec7f487fe29
m5ka <m5ka@posteo.de>
2024-03-25 15:35:07 +0000

    feature: 🍹 add drink verb

--- moku/constants.py
+++ moku/constants.py
@@ -3,12 +3,14 @@ from django.utils.translation import gettext_lazy as _
 
 class Verbs:
     ATE = "ate"
+    DRANK = "drank"
     MADE = "made"
     COOKED = "cooked"
     BAKED = "baked"
     ORDERED = "ordered"
     CHOICES = (
         (ATE, _("%(user)s ate %(food)s")),
+        (DRANK, _("%(user)s drank %(food)s")),
         (MADE, _("%(user)s made %(food)s")),
         (COOKED, _("%(user)s cooked %(food)s")),
         (BAKED, _("%(user)s baked %(food)s")),
